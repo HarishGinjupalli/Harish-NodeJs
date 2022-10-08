9902254307268fa265cec32c5e01ab11b0af3db7
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 12:12:15 +0530

    Add files via upload

---
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/index.js
@@ -0,0 +1,4 @@
+const { value1, name } = require("./sample");
+
+console.log(value1);
+console.log(name);
---
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/sample.js
@@ -0,0 +1,5 @@
+let value1 = 5;
+exports.value1 = value1;
+
+let name = "Harish";
+exports.name = name;
