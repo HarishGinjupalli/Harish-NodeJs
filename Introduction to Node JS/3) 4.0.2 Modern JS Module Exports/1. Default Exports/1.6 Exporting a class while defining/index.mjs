a507e9679ec5ce11564d2902f2101f838ff95f5d
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 12:28:15 +0530

    Add files via upload

--- Introduction to Node JS/3) 4.0.2 Modern JS Module Exports/1. Default Exports/1.6 Exporting a class while defining/index.mjs
+++ Introduction to Node JS/3) 4.0.2 Modern JS Module Exports/1. Default Exports/1.6 Exporting a class while defining/index.mjs
@@ -1,1 +1,5 @@
-
+import StudentDetails from "./sample.mjs";
+let newStudentDetails = new StudentDetails("hari", 23);
+console.log(newStudentDetails);
+console.log(newStudentDetails.name);
+//index.mjs
