d427db65f9b5ae1d328c681dd723f2b0da4a3dfe
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 12:38:59 +0530

    Add files via upload

--- Introduction to Node JS/3) 4.0.2 Modern JS Module Exports/2. Named Exports/2.6 Exporting multiple classes after defining/index.mjs
+++ Introduction to Node JS/3) 4.0.2 Modern JS Module Exports/2. Named Exports/2.6 Exporting multiple classes after defining/index.mjs
@@ -1,1 +1,9 @@
+import { StudentDetails, CarDetails } from "./sample.mjs";
 
+const newStudentDetails = new StudentDetails("hari", 29);
+console.log(newStudentDetails);
+console.log(newStudentDetails.name);
+
+const newCarDetails = new CarDetails("audi", 2900);
+console.log(newCarDetails);
+console.log(newCarDetails.name);
---
+++ Introduction to Node JS/3) 4.0.2 Modern JS Module Exports/2. Named Exports/2.6 Exporting multiple classes after defining/sample.mjs
@@ -0,0 +1,16 @@
+class StudentDetails {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class CarDetails {
+  constructor(name, model, color) {
+    this.name = name;
+    this.model = model;
+    this.color = color;
+  }
+}
+
+export { StudentDetails, CarDetails };
