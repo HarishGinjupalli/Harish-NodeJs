826cbb70b6e3d51247398eac323e31f98ad18461
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 12:14:18 +0530

    Add files via upload

--- Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/2.3 Exporting multiple values and expressions/index.js
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/2.3 Exporting multiple values and expressions/index.js
@@ -1,1 +1,7 @@
+//index.js
 
+const { sum, sub } = require("./sample");
+
+console.log(sum);
+
+console.log(sub);
---
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/2.3 Exporting multiple values and expressions/sample.js
@@ -0,0 +1,5 @@
+//sample.js
+
+let value = 2;
+exports.sum = 2 + 3;
+exports.sub = 3 - value;
