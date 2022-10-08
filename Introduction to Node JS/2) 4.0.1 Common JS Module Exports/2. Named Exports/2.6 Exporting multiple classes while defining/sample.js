708e53d54b43ce5e4fbea155f8dc61b7aa5d5e98
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 12:19:41 +0530

    Add files via upload

---
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/2.6 Exporting multiple classes while defining/sample.js
@@ -0,0 +1,16 @@
+exports.studentDetails = class StudentDetails {
+  constructor(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+};
+
+exports.carDetails = class CarDetails {
+  constructor(name, model, speed, color) {
+    this.name = name;
+    this.model = model;
+    this.speed = speed;
+    this.color = color;
+  }
+};
