ac758a904a343f09e074c278dd434f4271a9fdd5
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 11:45:53 +0530

    Add files via upload

--- Introduction to Node JS/2) 4.0.1 Common JS Module Exports/1. Default Exports/1.7 Exporting a class after defining/index.js
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/1. Default Exports/1.7 Exporting a class after defining/index.js
@@ -1,1 +1,11 @@
+const StudentDetails = require("./sample");
 
+const studentDetails = new StudentDetails("Harish", 23, "m");
+
+console.log(studentDetails);
+
+console.log(studentDetails.age);
+
+console.log(
+  `${studentDetails.name} , ${studentDetails.age} , ${studentDetails.sex}`
+);
---
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/1. Default Exports/1.7 Exporting a class after defining/sample.js
@@ -0,0 +1,9 @@
+class StudentDetails {
+  constructor(name, age, sex) {
+    this.name = name;
+    this.age = age;
+    this.sex = sex;
+  }
+}
+
+module.exports = StudentDetails;
