c268a8fe94972bfd2fd42182c5c56eb0f4e5cb4d
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 12:16:13 +0530

    Add files via upload

--- Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/2.4 Exporting multiple functions while defining/index.js
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/2.4 Exporting multiple functions while defining/index.js
@@ -1,1 +1,3 @@
+const { add, mul } = require("./sample");
 
+console.log(`add: ${add(2, 3)} , mul: ${mul(3, 6)}`);
---
+++ Introduction to Node JS/2) 4.0.1 Common JS Module Exports/2. Named Exports/2.4 Exporting multiple functions while defining/sample.js
@@ -0,0 +1,8 @@
+exports.add = function (num1, num2) {
+  return num1 + num2;
+};
+
+exports.mul = function (num1, num2) {
+  return num1 * num2;
+};
+//sample.js
