f767e317c1060e824b435a7cb5dbc66ecedb28ac
HarishGinjupalli <69430888+HarishGinjupalli@users.noreply.github.com>
2022-10-08 10:54:34 +0530

    Add files via upload

--- Introduction to Node JS | Part 2/Coding Practice-2/1) Get Date After x Days/index.js
+++ Introduction to Node JS | Part 2/Coding Practice-2/1) Get Date After x Days/index.js
@@ -1,12 +1,10 @@
-Get Date After x Days
+const addDays = require("date-fns/addDays");
 
-Given an index.js file parallel to README.md file.
+const getDateAfterXDays = (days) => {
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  return `${newDate.getDate()}-${
+    newDate.getMonth() + 1
+  }-${newDate.getFullYear()}`;
+};
 
-Write a JS function that accepts days as an argument and return the date after given number of days from 22nd Aug 2020 using the date-fns package.
-
-Export the function using the default export syntax.
-
-Date Format
-DD-MM-YYYY
-
-Use Common JS module syntax
+module.exports = getDateAfterXDays;
